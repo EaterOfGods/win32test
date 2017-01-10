4ded49a92be78cef51a96c4a3f576f6a995a00bd
Tony <nepra_84@msn.com>
2017-01-10 22:34:44 +0100

    Updated gameloop, added gradient feature

--- source/win32_Test.cpp
+++ source/win32_Test.cpp
@@ -9,50 +9,107 @@
 	===================================================================  */
 
 #include <windows.h>
+#include <stdint.h>
 
 #define global_variable static
 #define local_persist static
 #define internal static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+// TODO(Tony): don't use globals.
 global_variable bool Running;
+
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
+
+internal void
+RenderWeirdGradient(int XOffset, int YOffset)
+{
+	int Width = BitmapWidth;
+	int Height = BitmapHeight;
+
+	int Pitch = Width*BytesPerPixel;
+	uint8 *Row = (uint8 *)BitmapMemory;
+	for(int Y = 0;
+		Y < BitmapHeight; 
+		++Y)
+	{
+		uint8 *Pixel = (uint8 *)Row;
+
+		for(int X = 0; 
+			X < BitmapWidth; 
+			++X)
+		{
+			*Pixel = (uint8)(X + XOffset); 	//B
+			++Pixel;
+
+			*Pixel = (uint8)(Y + YOffset); 	//G
+			++Pixel;
+
+			*Pixel = 0;						//R		
+			++Pixel;
+
+			*Pixel = 0;						//A
+			++Pixel;
+		}
+
+		Row += Pitch;
+	}
+}
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
-	if(BitmapHandle)
+	if(BitmapMemory)
 	{
-		DeleteObject(BitmapHandle);
-	}
-	
-	if(!BitmapDeviceContext)
-	{
-		BitmapDeviceContext = CreateCompatibleDC(0);
+		VirtualFree(BitmapMemory, 0, MEM_RELEASE);
 	}
 
+	BitmapWidth = Width;
+	BitmapHeight = Height;
+
 	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-	BitmapInfo.bmiHeader.biWidth = Width;
-	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+	BitmapInfo.bmiHeader.biHeight = -BitmapHeight; // NOTE(Tony): Negative for top->down.
 	BitmapInfo.bmiHeader.biPlanes = 1;
 	BitmapInfo.bmiHeader.biBitCount = 32;
 	BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-	BitmapHandle = CreateDIBSection(
-		BitmapDeviceContext, &BitmapInfo,
-		DIB_RGB_COLORS,
-		&BitmapMemory,
-		0, 0); 
+	// NOTE(Tony): Using padding for memory alignment.
+
+	int BitmapMemorySize = (Width*Height)*BytesPerPixel;
+	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+	RenderWeirdGradient(0, 0);
+
+	// TODO(Tony): Clear this to black.
 } 
 
 internal void
-Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height)
 {
+	int WindowWidth = WindowRect->right - WindowRect->left;
+	int WindowHeight = WindowRect->bottom - WindowRect->top;
+
 	StretchDIBits(DeviceContext,
+				  /*
 				  X, Y, Width, Height,
 				  X, Y, Width, Height,
+				  */
+				  0, 0, BitmapWidth, BitmapHeight,
+				  0, 0, WindowWidth, WindowHeight,
 				  BitmapMemory,
 				  &BitmapInfo,
 				  DIB_RGB_COLORS, SRCCOPY);
@@ -64,7 +121,7 @@ Win32MainWindowCallback(HWND Window,
 				   WPARAM WParam,
 				   LPARAM LParam)
 {
-	LRESULT Result = 0;
+	LRESULT Result = 0; 
 
 	switch(Message)
 	{
@@ -102,7 +159,12 @@ Win32MainWindowCallback(HWND Window,
 			int Y = Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
 			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-			Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+
+			// TODO(Tony): fix this shit.	
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+
+			Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
 			EndPaint(Window, &Paint);
 		} break;
 
@@ -154,15 +216,15 @@ WinMain(HINSTANCE Instance,
 			while(Running)
 			{
 				MSG Message;
-				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-				if(MessageResult > 0)
+				while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
 				{
+					if(Message.message == WM_QUIT)
+					{
+						Running = false;
+					}
+
 					TranslateMessage(&Message);
 					DispatchMessage(&Message);
-				}
-				else
-				{
-					break;
 				}
 			}
 		}
